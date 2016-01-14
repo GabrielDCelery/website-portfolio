c2d204a2833b55306f2a42f20251ddac5794aa90
Gabriel D. Celery <gabriel.d.celery@gmail.com>
2016-01-14 12:29:27 +0100

    Changd the icon behaviour, now when someone is hovering them over it also changes text/image display accordingly

--- public/js/app.js
+++ public/js/app.js
@@ -219,16 +219,28 @@ var icon = '.percentage-circle';
 EVENT BINDERS
 ********************************************************************************/
 
-	$renderedSkillsContainer.on('click', icon, function(){
-		var $spanElements = $(this).find('span');
-		$spanElements.toggle();
-	});
+	$renderedSkillsContainer.on('click', icon, toggleImage);
 
-	$renderedToolsContainer.on('click', icon, function(){
-		var $spanElements = $(this).find('span');
-		$spanElements.toggle();
-	});
+	$renderedSkillsContainer.on('mouseenter', icon, toggleImage);
 
+	$renderedSkillsContainer.on('mouseleave', icon, toggleImage);
+
+	$renderedToolsContainer.on('click', icon, toggleImage);
+
+	$renderedToolsContainer.on('mouseenter', icon, toggleImage);
+
+	$renderedToolsContainer.on('mouseleave', icon, toggleImage);
+
+/********************************************************************************
+FUNCTIONS
+********************************************************************************/
+
+function toggleImage(event){
+
+	var $spanElements = $(this).find('span');
+	$spanElements.toggle();
+
+}
 
 
 }());
